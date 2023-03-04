c77631b3d3a30a27c84d5b6aa947e7fbb9806220
Bekarys <bekarysnurgassym@gmail.com>
2023-03-05 00:05:44 +0600

    angular

---
+++ angular/store-app/src/app/cart.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core';
+import { Product } from './products';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+	providedIn: 'root',
+})
+
+export class CartService {
+	items: Product[] = [];
+	constructor(
+		private http: HttpClient
+	) {}
+
+	getShippingPrices() {
+		return this.http.get<{type: string, price: number}[]>('/assets/shipping.json');
+	}
+
+	addToCart(item: Product) {
+		this.items.push(item);
+	}
+
+	getItems() {
+		return this.items;
+	}
+
+	clearCart() {
+		this.items = [];
+		return this.items;
+	}
+}
